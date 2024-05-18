001c756aa5d42d68625061e7c96058b3b38f239c
Pradeep sahu <sahup2504@gmail.com>
2024-05-18 08:13:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from win32com.client import Dispatch
+print("Wellcome to Speak")
+print("NOTE :- enjoy in program and  exit to press Z key to enter \n Thankyou")
+while True:
+    x = input("Enter what you want to me to speak:   ")
+    speak = Dispatch("SAPI.SpVoice").Speak
+    if x == "z":
+        speak("Nice to meet u thankyou by sir")
+        break
+    speak(x)
+print("by by")
